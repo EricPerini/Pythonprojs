4d61e9e7c67ec75625fbc450860bcf937a3d3eb0
Eric Fernandes Perine <145853176+EricPerine@users.noreply.github.com>
2023-11-03 20:43:33 -0300

    Add files via upload

---
+++ Cifra de cesar^-1.py
@@ -0,0 +1,17 @@
+alfabeto = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8','9', '0']
+word = input('Frase a ser descriptografada: ')
+key = (int(input('Chave: '))%36)
+word1 = list(word)
+word2 = []
+print(word1)
+for i in range(len(word1)):
+    for j in range(len(alfabeto)):
+        if word1[i] == (alfabeto[j]):
+            print('yes', i, j)
+            word2.append(alfabeto[j-key])
+            print(word2)
+        elif word1[i]== ' ':
+                word2.append(' ')
+                break
+word3 = ''.join(word2)
+print('frase descriptografada: ', word3)
