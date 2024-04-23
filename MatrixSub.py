a28f207fda8010793a2e7410e5a09a95c854ba9b
Eric Fernandes Perini <145853176+EricPerine@users.noreply.github.com>
2024-04-23 10:43:57 -0300

    Add files via upload

---
+++ MatrixSub.py
@@ -0,0 +1,28 @@
+import MatrixOperations
+print('Matriz 1')
+LineQunt_m1 = int(input('Quantas linhas: '))
+ClumQunt_m1 = int(input('Quantas Colunas: '))
+print('Matriz 1')
+M1 = []
+M1_aux = []
+for i in range(LineQunt_m1):
+    for j in range(ClumQunt_m1):
+        x = int(input(f'Elemento da Linha {i+1} e coluna {j+1}: '))
+        M1_aux.append(x)
+    M1.append(M1_aux[:])
+    M1_aux.clear()
+print('Matriz 2')
+M2 = []
+M2_aux = []
+for i in range(LineQunt_m1):
+    for j in range(ClumQunt_m1):
+        y = int(input(f'Elemento da Linha {i+1} e coluna {j+1}: '))
+        M2_aux.append(y)
+    M2.append(M2_aux[:])
+    M2_aux.clear()
+MR = MatrixOperations.MtrxCrt(LineQunt_m1, ClumQunt_m1)
+for i in range(LineQunt_m1):
+    for j in range(ClumQunt_m1):
+        MR[i][j] = float(M1[i][j])-float(M2[i][j])
+for row in range(LineQunt_m1):
+    print(MR[row])
